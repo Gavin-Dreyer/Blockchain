e6db0f08413f6eb5fb8ad6a389dd97c36100f69e
Gavin Dreyer <gavindreyer1@gmail.com>
2020-03-12 14:49:56 -0700

    Now keeps track of coins sent and recieved outside of mining

--- blockchain_client/src/App.js
+++ blockchain_client/src/App.js
@@ -2,70 +2,102 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import axios from 'axios'
-import { resetWarningCache } from 'prop-types';
+import axios from 'axios';
 
 function App() {
-  const [chain, setChain] = useState()
-  const [recipient, setRecipient] = useState()
-  const [id, setId] = useState({ id: '' })
+	const [chain, setChain] = useState();
+	const [recipient, setRecipient] = useState();
+	const [currentAccount, setCurrentAccount] = useState();
+	const [transactions, setTransactions] = useState();
+	const [id, setId] = useState({ id: '' });
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5000/chain').then(res => {
-      setChain(res.data.chain)
-    }).catch(err => {
-      console.log(err)
-    })
-  }, [])
+	useEffect(() => {
+		axios
+			.get('http://127.0.0.1:5000/chain')
+			.then(res => {
+				res.data.chain.forEach(item => {
+					if (item.transactions.length > 0) {
+						item.transactions.forEach(item2 => {
+							item2.amount = Number(item2.amount);
+						});
+					}
+				});
+				setChain(res.data.chain);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
 
-  const handleChange = e => {
-    setId({ ...id, [e.target.name]: e.target.value })
-  }
+	const handleChange = e => {
+		setId({ ...id, [e.target.name]: e.target.value });
+	};
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    let filteredChain = chain.filter(item => {
-      if (item.transactions.length > 0 && item.transactions[0].recipient === id.id) {
-        return item
-      }
-    })
-    setRecipient(filteredChain)
-    setId({ id: '' })
-  }
+	const handleSubmit = e => {
+		e.preventDefault();
+		let arr = [];
+		chain.forEach(item => {
+			if (item.transactions.length > 0) {
+				arr = [...arr, ...item.transactions];
+				// setTransactions([...transactions, ...item.transactions])
+			}
+		});
+		let filteredChain = chain.filter(item => {
+			if (
+				item.transactions.length > 0 &&
+				item.transactions[0].recipient === id.id
+			) {
+				return item;
+			}
+		});
+		setTransactions(arr);
+		setRecipient(filteredChain);
+		setCurrentAccount(id.id);
+		setId({ id: '' });
+	};
 
-  console.log(recipient)
-  return (
-    <div className="App">
-      <form onSubmit={e => handleSubmit(e)}>
-        <input
-          type="text"
-          name="id"
-          value={id.id}
-          onChange={e => handleChange(e)}
-        />
-      </form>
-      <div>
-        {recipient ? recipient.reduce((acc, val) => {
-          return acc + val.transactions[0].amount
-        }, 0) : ''}
-      </div>
-      <div>
-        {recipient ? recipient.map(item => {
-          return <div key={item.index}>
-            <p>
-              Block: {item.index}
-            </p>
-            <p>
-              Time: {item.timestamp}
-            </p>
-            <p>
-              Coins: {item.transactions[0].amount}
-            </p>
-          </div>
-        }) : ''}
-      </div>
-    </div>
-  );
+	console.log(transactions);
+	return (
+		<div className="App">
+			<form onSubmit={e => handleSubmit(e)}>
+				<input
+					type="text"
+					name="id"
+					value={id.id}
+					onChange={e => handleChange(e)}
+				/>
+			</form>
+			<div>{currentAccount}</div>
+			<div>
+				Current Amount:{' '}
+				{transactions
+					? transactions.reduce((acc, val) => {
+							if (val.recipient === currentAccount) {
+								return acc + val.amount;
+							} else if (val.sender === currentAccount) {
+								return acc - val.amount;
+							} else {
+								return acc;
+							}
+					  }, 0)
+					: ''}
+			</div>
+			<div>
+				Blocks Mined: {recipient ? recipient.length : ''}
+				{recipient
+					? recipient.map(item => {
+							return (
+								<div key={item.index}>
+									<p>Block: {item.index}</p>
+									<p>Time: {item.timestamp}</p>
+									<p>Coins: {item.transactions[0].amount}</p>
+								</div>
+							);
+					  })
+					: ''}
+			</div>
+		</div>
+	);
 }
 
 export default App;
